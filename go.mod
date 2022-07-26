2a086d07e70276697dd8cdb917398f66d0591881
netbyte <alex.tsai@protonmail.com>
2022-07-26 22:10:38 +0800

    init

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/net-byte/water
+
+go 1.18
+
+require (
+	golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f
+	golang.zx2c4.com/wireguard v0.0.0-20220703234212-c31a7b1ab478
+)
+
+require (
+	golang.org/x/net v0.0.0-20220225172249-27dd8689420f // indirect
+	golang.zx2c4.com/wintun v0.0.0-20211104114900-415007cec224 // indirect
+)
---
+++ syscalls_windows.go
@@ -0,0 +1,41 @@
+package water
+
+import (
+	"golang.org/x/sys/windows"
+	"golang.zx2c4.com/wireguard/tun"
+)
+
+type wintun struct {
+	dev tun.Device
+}
+
+func (w *wintun) Close() error {
+	return w.dev.Close()
+}
+
+func (w *wintun) Write(b []byte) (int, error) {
+	return w.dev.Write(b, 0)
+}
+
+func (w *wintun) Read(b []byte) (int, error) {
+	return w.dev.Read(b, 0)
+}
+
+func openDev(config Config) (ifce *Interface, err error) {
+	if config.DeviceType == TAP {
+		return nil, err
+	}
+	id := &windows.GUID{
+		0x0000000,
+		0xFFFF,
+		0xFFFF,
+		[8]byte{0xFF, 0xe9, 0x76, 0xe5, 0x8c, 0x74, 0x06, 0x3e},
+	}
+	dev, err := tun.CreateTUNWithRequestedGUID(config.PlatformSpecificParams.InterfaceName, id, 0)
+	if err != nil {
+		return nil, err
+	}
+	wintun := &wintun{dev: dev}
+	ifce = &Interface{isTAP: (config.DeviceType == TAP), ReadWriteCloser: wintun}
+	return ifce, nil
+}
